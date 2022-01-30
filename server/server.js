7b991e3deb92c985456976d35e31e50eb7a8cacf
ccroberts1 <caitlin.coy.roberts@gmail.com>
2022-01-30 13:06:28 -0700

    added initial schemas files and configured new server.js for apollo server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
-const app = express();
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function startServer() {
+  const server = (apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  }));
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startServer();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
